bc857037cda6fd2f315083891a92bfa7eec63e33
NikolaKrtinic <krtans@yahoo.com>
2021-07-27 00:13:50 +0200

    added search news logic

--- src/components/SearchNews.js
+++ src/components/SearchNews.js
@@ -1,21 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { Input } from '@material-ui/core';
 
-const SearchNews = () =>{
+import {useDispatch, useSelector} from 'react-redux'
+import {getNews} from '../actions/newsAction'
+
+const SearchNews = ({country}) =>{
 
     const [term, setTerm] = useState('');
+    const [category, setCategory] = useState('');
 
+    const dispatch = useDispatch()
+    const newsList = useSelector(state => state.newsList)
+    const {loading, error, news} = newsList
+
+    function onFormSubmit(e){
+        e.preventDefault();
+    }
     useEffect(() => {
-
-    });
+        const timer = setTimeout(() => {
+            if(term){
+                dispatch(getNews(country,category,term))
+            }  
+          }, 1000);
+          return () => clearTimeout(timer);  
+      }, [dispatch, country,category,term])
 
     return(
         <div>
-            <Input
-                placeholder='Search term'
-                value={term}
-                onChange={(e) => setTerm(e.target.value)}
-            />
+            <form onSubmit={(e) => onFormSubmit(e)}>
+                <Input
+                    placeholder='Search term'
+                    value={term}
+                    onChange={(e) => setTerm(e.target.value)}
+                />
+            </form>
         </div>
     );
 };
